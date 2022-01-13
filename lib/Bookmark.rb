a3a496834092e23a430cf30f1c6b625ce2d55e0f
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-13 12:22:35 +0000

    Update view to render title with link to url

--- lib/Bookmark.rb
+++ lib/Bookmark.rb
@@ -12,16 +12,10 @@ class Bookmark
     rows.each do |row| 
       bookmarks[row['title']] = row['url']
     end
-    p bookmarks # this is not what we want to return now. 
-    # I think the tests need adjusting to reflect the information 
-    # that we want
-    # we want the titles rendered on the page, but each one has 
-    # the url as a clickable link. We looked at hashes but also
-    # could look at arrays.
+    bookmarks
   end 
 
   def self.create(url, title)
-    @title = title
 
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect :dbname => 'bookmark_manager_test'
@@ -31,7 +25,4 @@ class Bookmark
     connection.exec "INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}');"
   end
 
-  def self.title
-    @title
-  end
 end 
--- spec/features/see_bookmarks_spec.rb
+++ spec/features/see_bookmarks_spec.rb
@@ -1,13 +1,12 @@
 feature "See all bookmarks" do
-  before do
+  
+  scenario 'users can see all titles' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
     connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.makersacademy.com', 'Makers Academy')")
     connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'Google')")
     connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.destroyallsoftware.com', 'Destroy All Software')")
-  end
-  
-  scenario 'users can see all titles' do
+
     visit ('/bookmarks')
       expect(page).to have_content("Makers Academy")
       expect(page).to have_content("Destroy All Software")
